f51b817f55c24bfff7a2657cfd24728bd818e03c
rmwdcsur <risso.santiago@gmail.com>
2026-04-28 23:11:47 -0300

    __dirname + format

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __dirname = resolve(fileURLToPath(import.meta.url), "..");
 
 export default defineConfig({
   root: "src/",
@@ -13,16 +16,16 @@ export default defineConfig({
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product1: resolve(
           __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html"
+          "src/product_pages/cedar-ridge-rimrock-2.html",
         ),
         product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
         product3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html"
+          "src/product_pages/northface-alpine-3.html",
         ),
         product4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html"
+          "src/product_pages/northface-talus-4.html",
         ),
       },
     },
